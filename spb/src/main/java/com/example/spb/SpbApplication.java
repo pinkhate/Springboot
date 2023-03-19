09b6f5338227e064347e7ee6fd9e7aa530c40240
pinkhate <2471588682@qq.com>
2023-03-19 10:50:52 +0800

    springboot-增删改查

--- spb/src/main/java/com/example/spb/SpbApplication.java
+++ spb/src/main/java/com/example/spb/SpbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpbApplication {
-
+    //实现简单的增删改查
     public static void main(String[] args) {
         SpringApplication.run(SpbApplication.class, args);
     }
