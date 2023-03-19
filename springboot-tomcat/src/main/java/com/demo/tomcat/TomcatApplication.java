989f9ee145152954c215e43dca44cefcdc58acbe
pinkhate <2471588682@qq.com>
2023-03-19 10:59:15 +0800

    springboot-tomcat配置
    Signed-off-by: pinkhate <2471588682@qq.com>

--- springboot-tomcat/src/main/java/com/demo/tomcat/TomcatApplication.java
+++ springboot-tomcat/src/main/java/com/demo/tomcat/TomcatApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TomcatApplication {
 
+    //tomcat配置
     public static void main(String[] args) {
         SpringApplication.run(TomcatApplication.class, args);
     }
