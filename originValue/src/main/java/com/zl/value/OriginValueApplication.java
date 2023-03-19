233af0761e4bafdb8813e4fdfe75f9760db85852
pinkhate <2471588682@qq.com>
2023-03-19 11:47:42 +0800

    springboot-属性注入
    Signed-off-by: pinkhate <2471588682@qq.com>

--- originValue/src/main/java/com/zl/value/OriginValueApplication.java
+++ originValue/src/main/java/com/zl/value/OriginValueApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OriginValueApplication {
 
+    //属性注入
     public static void main(String[] args) {
         SpringApplication.run(OriginValueApplication.class, args);
     }
