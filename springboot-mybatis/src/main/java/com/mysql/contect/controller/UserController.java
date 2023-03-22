6c058e9be7b6b091ff348164681bf66b2d78ca5e
pinkhate <2471588682@qq.com>
2023-03-22 20:32:28 +0800

    springboot-mybatis整合esayexcel实现
    Signed-off-by: pinkhate <2471588682@qq.com>

--- springboot-mybatis/src/main/java/com/mysql/contect/controller/UserController.java
+++ springboot-mybatis/src/main/java/com/mysql/contect/controller/UserController.java
@@ -4,7 +4,6 @@ import com.alibaba.excel.EasyExcel;
 import com.mysql.contect.model.User;
 import com.mysql.contect.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,16 +21,16 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    UserService  userService;
+    UserService userService;
 
     @GetMapping("/export")
     public void exportExcel(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        List<User> list =  userService.getAllUsers();
+        List<User> list = userService.getAllUsers();
 
         resp.setHeader("content-disposition",
-                "attachment;filename="+new String("用户数据.xlsx".getBytes("UTF-8"),"ISO8859-1"));
-    EasyExcel.write(resp.getOutputStream(), User.class).sheet("用户数据").doWrite(list);
+                "attachment;filename=" + new String("用户数据.xlsx".getBytes("UTF-8"), "ISO8859-1"));
+        EasyExcel.write(resp.getOutputStream(), User.class).sheet("用户数据").doWrite(list);
 
 
     }
